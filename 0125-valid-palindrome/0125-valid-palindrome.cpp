5b26863216af583fe394687fc6413a5414990516
Keshav <keshav456goyal@gmail.com>
2026-07-18 11:04:55 +0530

    Time: 0 ms (100%), Space: 10 MB (54.89%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int low=0;
+        int high=s.size()-1;
+
+        while(low<high){
+            if(!isalnum(s[low])){
+                low++;
+                continue;
+            } 
+            if(!isalnum(s[high])){
+                high--;
+                continue;
+            }
+
+            if(tolower(s[low]) == tolower(s[high])){
+                low++;
+                high--;
+            }else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
