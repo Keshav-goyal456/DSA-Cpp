4942c34364591857439f7ae07f199116d6ec044b
Keshav <keshav456goyal@gmail.com>
2026-07-25 09:24:55 +0530

    Time: 48 ms (53.92%), Space: 29 MB (92.67%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,115 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> ans;
+
+        for(int i=0;i<n;i++){
+
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            int j=i+1;
+            int k=n-1;
+
+            while(j<k){
+                int sum= nums[i]+nums[j]+nums[k];
+
+                if(sum>0){
+                    k--;
+                }else if(sum<0){
+                    j++;
+                } else{
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+
+                    while(j<k && nums[j] == nums[j-1]){
+                        j++;
+                    }
+
+                    while(j<k && nums[k] == nums[k+1]){
+                        k--;
+                    }
+                    
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<vector<int>> threeSum(vector<int>& nums) {
+//         int n=nums.size();
+
+//         set<vector<int>> triplets;
+
+//         for(int i=0;i<n;i++){
+//             int target=-nums[i];
+//             set<int> s;
+
+//             for(int j=i+1;j<n;j++){
+//                 int third= target-nums[j];
+
+//                 if(s.find(third) != s.end()){
+//                     vector<int> temp = {nums[i], nums[j], third};
+//                     sort(temp.begin(), temp.end());
+
+//                     triplets.insert(temp);
+//                 }
+//                 s.insert(nums[j]);
+//             }
+//         }
+        
+//         vector<vector<int>> ans(triplets.begin(), triplets.end());
+//         return ans;
+//     }
+// };
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<vector<int>> threeSum(vector<int>& nums) {
+
+//         int n=nums.size();
+
+//         set<vector<int>> st;
+
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 for(int k=j+1;k<n;k++){
+//                         if(nums[i]+nums[j]+nums[k]==0){
+
+//                         vector<int> temp = {nums[i], nums[j], nums[k]};
+
+//                         sort(temp.begin(), temp.end());
+
+//                         st.insert(temp);
+
+//                         }
+//                 }
+//             }
+//         }
+
+//         vector<vector<int>> ans(st.begin(), st.end());
+
+//         return ans;
+//     }
+// };
